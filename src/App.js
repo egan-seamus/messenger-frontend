3b99200a8fb667c2d4c7f08044e3670a9674e281
Seamus Egan <egan.seam@husky.neu.edu>
2021-02-11 21:08:04 -0500

    changed code to test register functionality

--- src/App.js
+++ src/App.js
@@ -6,35 +6,44 @@ import axios from 'axios';
 
 const baseURL = "http://localhost:8000/"
 const loginURL = "messaging/login/"
+const registerURL = "messaging/register/"
 
 class LoginForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username : "",
-      password : ""
+      username: "",
+      password: ""
     }
   }
 
-  handleUsernameChange =(e) => {
-    this.setState({username : e.target.value});
+  handleUsernameChange = (e) => {
+    this.setState({ username: e.target.value });
   }
 
-  handlePasswordChange =(e) => {
-    this.setState({password : e.target.value});
+  handlePasswordChange = (e) => {
+    this.setState({ password: e.target.value });
   }
 
   handleSubmitButton(e) {
     e.preventDefault();
     console.log("Username: ", this.state.username);
     console.log("Password: ", this.state.password);
-    axios.post(baseURL.concat(loginURL), {
+    axios.post(baseURL.concat(registerURL), {
       username : this.state.username,
       password : this.state.password
     })
-    .then((response) => {
-      console.log(response);
-    });
+    // const options = {
+    //   method: 'POST',
+    //   body: JSON.stringify({
+    //     username: this.state.username,
+    //     password: this.state.password
+    //   })
+    // }
+    // fetch(baseURL.concat(loginURL), options)
+      .then((response) => {
+        console.log(response);
+      });
   }
 
   render() {
@@ -42,12 +51,12 @@ class LoginForm extends React.Component {
       <Form>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="username" placeholder="Enter username"  onChange={(e) => this.handleUsernameChange(e)}/>
+          <Form.Control type="username" placeholder="Enter username" onChange={(e) => this.handleUsernameChange(e)} />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" onChange={(e) => this.handlePasswordChange(e)}/>
+          <Form.Control type="password" placeholder="Password" onChange={(e) => this.handlePasswordChange(e)} />
         </Form.Group>
         <Button variant="primary" type="submit" onClick={(e) => this.handleSubmitButton(e)}>
           Submit
